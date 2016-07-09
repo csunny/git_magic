89eddcc7a1a2054ad722152d8adcb58752e39e2f
sunny <vfqsunny@163.com>
2016-07-09 19:20:57 +0800

    img

--- js-dom/js/lesson4.js
+++ js-dom/js/lesson4.js
@@ -6,4 +6,13 @@ function showpic(whichpic){
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
+    var text = whichpic.getAttribute("title");
+    var discription = document.getElementById("discription");
+    discription.firstChild.nodeValue = text;
+
 }
+function countBodyChild(){
+    var body_elcment = document.getElementsByTagName("body")[0];
+    console.log(body_elcment.childNodes.length);
+}
+window.onload = countBodyChild();
