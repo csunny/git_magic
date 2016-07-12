552dbe3b1f839fe0fa605ce7b9b7cfc3aaefb2e0
magic <magic.chen@fuwo.com>
2016-07-12 20:05:43 +0800

    fix

--- js-function/js/1-1.js
+++ js-function/js/1-1.js
@@ -39,4 +39,50 @@ var factorial = (function f(num) {
     }
 })
 
+//闭包
 
+function createComparisonFunction(propertyName) {
+    return function (object1, object2) {
+        var value1 = object1[propertyName];
+        var value2 = object2[propertyName];
+
+        if(value1 < value2){
+            return -1;
+
+        }else{
+            return 0;
+        }
+    }
+}
+
+/*当某个函数被调用时，会创建一个执行环境，execution context*及相应的作用域链，然后，使用argument和其他命名参数的值来初始化函数的活动
+对象，但在作用域链中，外部函数的活动对象始终处于第二位，外部函数的活动对象处于第三位/
+ */
+
+function compare(value1, value2){
+    if(value1 < value2){
+        return -1;
+    }else if(value1 > value2){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+var result = compare(5, 10);
+
+
+// 闭包与变量
+function createFunctions() {
+    var result = new Array()
+    for(var i=0; i<10; i++){
+        result[i] = function () {
+            return i;
+        }
+
+        return result;
+    }
+}
+
+
+//
