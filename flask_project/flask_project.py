ab5489010890cb7538020ab153bd05d07e7c4413
csunny <cfqsunny@163.com>
2016-09-16 15:23:45 +0800

    code style

--- flask_project/flask_project.py
+++ flask_project/flask_project.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html', title='Welcome')
 
+
 @app.route('/service')
 def service():
     return service
+
 
 @app.route('/about')
 def about():
